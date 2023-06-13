851aeef0ff77f53de14bca87ec4f2ee00d6b713c
shusanket <shusanketbasyal76@gmail.com>
2023-06-13 12:39:17 -0400

    BugFixed

--- backend/settings.py
+++ backend/settings.py
@@ -23,12 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
-print(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
